4756f2d0d325295c729017381e096de081c7cede
Jacob Brown <jacob@gnu.org>
2018-04-02 08:25:33 -0500

    Adding my mouse and keyboard scripts

---
+++ .keyboard/stuff.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#xmodmap /home/jacob/.keyboard/2
+setxkbmap -option ctrl:nocaps
+xset r rate 240 44
---
+++ .mouse/logitech-mouse.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xinput --set-prop 'Logitech USB Optical Mouse' 'Coordinate Transformation Matrix' 1.100000, 0.000000, 0.000000, 0.000000, 1.100000, 0.000000, 0.000000, 0.000000, 1.000000
+xinput --set-prop 'Logitech USB Optical Mouse' 'libinput Accel Profile Enabled' 0, 1
+
